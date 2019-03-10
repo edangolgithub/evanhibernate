07fda1c1b657c60f3ffa8904acf14cf25ee60c19
Niraj <Niraj@DESKTOP-5JLFP0L>
2019-03-10 10:51:25 +0545

    hi

--- src/evanhibernate/CategoryStart.java
+++ src/evanhibernate/CategoryStart.java
@@ -9,7 +9,7 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class CategoryStart
-	{
+	{ 
             static  SessionFactory factory=null;
 		public static void main(String[] args)
 			{
